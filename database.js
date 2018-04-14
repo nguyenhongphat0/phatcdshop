2c76b9e103c3e59cf66cf729e919a9347336e2f2
nguyenhongphat0 <nguyenhongphat28121998@gmail.com>
2018-04-14 17:37:48 +0000

    publishing

---
+++ database.js
@@ -0,0 +1,121 @@
+var gamelist = new Array;
+
+//add gta5
+var gta5 = new Game("GTA5", "599000", "31GB", "FPS", "17/9/2013", "Rockstar", 
+        "CPU: Intel Core i3-550 | AMD Phenom II X4 955 or equivalent <br>\n RAM: 4 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 460 (1GB VRAM) | AMD Radeon HD 5770 (1GB VRAM) or equivalent <br>\n Hard Drive: 20 GB", 
+        "CPU: Intel Core i7-2600K | AMD FX-8350 or equivalent <br>\n RAM: 8 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 780 | AMD Radeon R9 280X or equivalent <br>\nHard Drive: 20 GB", 
+        "GTA (viết tắt của Grand Theft Auto) là một sêri trò chơi được sản xuất tại Anh bởi Dave Jones, sau này là hai anh em người Anh Dan Houser và Sam Houser, cùng với nhà thiết kế trò chơi Zachary Clarke",
+        "gta/pic1.jpg","gta/pic2.jpg","gta/pic3.jpg","gta/pic4.jpg");
+gamelist.push(gta5);
+
+//add starcraft2
+var sc2 = new Game("Starcraft 2", "299000", "20GB", "RTS", "27/7/2010", "Blizzard",
+        "CPU: Intel Core i3-550 | AMD Phenom II X4 955 or equivalent <br>\n RAM: 4 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 460 (1GB VRAM) | AMD Radeon HD 5770 (1GB VRAM) or equivalent <br>\n Hard Drive: 20 GB",
+        "CPU: Intel Core i7-2600K | AMD FX-8350 or equivalent <br>\n RAM: 8 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 780 | AMD Radeon R9 280X or equivalent <br>\nHard Drive: 20 GB",
+        "Thiết lập vào thế kỷ 26 trong một dải Ngân hà xa xôi, trò chơi xoay quanh 3 chủng loài: Terran, con người lưu vong từ Trái Đất; Zerg, một siêu chủng loài của các dạng sống đồng hóa; và Protoss, một loài sở hữu sức mạnh tâm linh. Wings of Liberty tập trung vào Terran, trong khi hai bản mở rộng Heart of the Swarm và Legacy of the Void sẽ lần lược tập trung vào Zerg và Protoss. Trò chơi lấy bối cảnh 4 năm sau các sự kiện của StarCraft: Brood War năm 1998, kể tiếp câu chuyện của Jim Raynor khi ông dẫn dắt một cuộc nổi dậy chống lại đế chế Terran Dominion. Trò chơi bao gồm các nhân vật và các địa điểm mới và cũ từ trò chơi gốc.",
+        "sc2/pic1.jpg", "sc2/pic2.jpg", "sc2/pic3.jpg", "sc2/pic4.jpg");
+gamelist.push(sc2);
+
+//add farcry3
+var fc3 = new Game("Far Cry 3", "199000", "12GB", "FPS", "28/11/2012", "Ubisoft", 
+        "CPU: Intel Core i3-550 | AMD Phenom II X4 955 or equivalent <br>\n RAM: 4 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 460 (1GB VRAM) | AMD Radeon HD 5770 (1GB VRAM) or equivalent <br>\n Hard Drive: 20 GB", 
+        "CPU: Intel Core i7-2600K | AMD FX-8350 or equivalent <br>\n RAM: 8 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 780 | AMD Radeon R9 280X or equivalent <br>\nHard Drive: 20 GB", 
+        "Far Cry 3 là trò chơi điện tử phiêu lưu hành động và bắn súng góc nhìn thứ nhất được phát triển chủ yếu bởi Ubisoft Montreal kết hợp với Ubisoft Massive, Ubisoft Red Storm, Ubisoft Thượng Hải, và Ubisoft Reflections.",
+        "fc3/pic1.jpg","fc3/pic2.jpg","fc3/pic3.jpg","fc3/pic4.jpg");
+gamelist.push(fc3);
+
+//add megamanx8
+var mg8 = new Game("Mega Man X8", "299000", "1.2GB", "2DP", "28/11/2012", "CAPCOM", 
+        "CPU: Intel Core i3-550 | AMD Phenom II X4 955 or equivalent <br>\n RAM: 4 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 460 (1GB VRAM) | AMD Radeon HD 5770 (1GB VRAM) or equivalent <br>\n Hard Drive: 20 GB", 
+        "CPU: Intel Core i7-2600K | AMD FX-8350 or equivalent <br>\n RAM: 8 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 780 | AMD Radeon R9 280X or equivalent <br>\nHard Drive: 20 GB", 
+        "Mega Man (hay Rock Man ở Nhật) là loạt trò chơi điện tử của Capcom, với nhân vật chính mang tên Mega Man. Phát hành lần đầu năm 1987 trên hệ máy Nintendo Entertainment System, Mega Man là loạt trò chơi được sản xuất nhiều nhất của Capcom vơi hơn 50 lần phát hành.",
+        "mg8/pic1.jpg","mg8/pic2.jpg","mg8/pic3.jpg","mg8/pic4.jpg");
+gamelist.push(mg8);
+
+//add streetfighter5
+var sf5 = new Game("Street Fighter V", "799000", "12GB", "2DP", "28/11/2016", "CAPCOM", 
+        "CPU: Intel Core i3-550 | AMD Phenom II X4 955 or equivalent <br>\n RAM: 4 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 460 (1GB VRAM) | AMD Radeon HD 5770 (1GB VRAM) or equivalent <br>\n Hard Drive: 20 GB", 
+        "CPU: Intel Core i7-2600K | AMD FX-8350 or equivalent <br>\n RAM: 8 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 780 | AMD Radeon R9 280X or equivalent <br>\nHard Drive: 20 GB", 
+        "Chiến Binh Đường Phố, là một trong những loại game đối kháng nổi tiếng trên toàn thế giới với cách chiến đấu tay đôi và một chọi một.",
+        "sf5/pic1.jpg","sf5/pic2.jpg","sf5/pic3.jpg","sf5/pic4.jpg");
+gamelist.push(sf5);
+
+//add plants vs zoombies
+var pvz = new Game("Plants vs. Zombies", "99000", "50MB", "RTS", "28/11/2008", "POPCAP", 
+        "CPU: Intel Core i3-550 | AMD Phenom II X4 955 or equivalent <br>\n RAM: 4 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 460 (1GB VRAM) | AMD Radeon HD 5770 (1GB VRAM) or equivalent <br>\n Hard Drive: 20 GB", 
+        "CPU: Intel Core i7-2600K | AMD FX-8350 or equivalent <br>\n RAM: 8 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 780 | AMD Radeon R9 280X or equivalent <br>\nHard Drive: 20 GB", 
+        "Plants vs Zombies là một trò chơi điện tử dạng thủ thành được PopCap Games phát triển và phát hành lần đầu cho hệ điều hành Microsoft Windows và OS X. Về mặt tổng quan người chơi sẽ sắm vai chủ một ngôi nhà và sử dụng các loại cây khác nhau để ngăn chặn đội quân zombie đột nhập và ăn mất não của mình",
+        "pvz/pic1.jpg","pvz/pic2.jpg","pvz/pic3.jpg","pvz/pic4.jpg");
+gamelist.push(pvz);
+
+//add ano 2070
+var ano = new Game("Anno 2070", "499000", "5GB", "RTS", "28/11/2013", "Ubisoft", 
+        "CPU: Intel Core i3-550 | AMD Phenom II X4 955 or equivalent <br>\n RAM: 4 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 460 (1GB VRAM) | AMD Radeon HD 5770 (1GB VRAM) or equivalent <br>\n Hard Drive: 20 GB", 
+        "CPU: Intel Core i7-2600K | AMD FX-8350 or equivalent <br>\n RAM: 8 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 780 | AMD Radeon R9 280X or equivalent <br>\nHard Drive: 20 GB", 
+        "Ano 2070 là trò chơi xây dựng và mô phỏng kinh tế chiến lược thời gian thực được phát triển bởi Ubisoft vào năm 2013. Là thế hệ thứ 5 của tựa game cùng tên",
+        "ano/pic1.jpg","ano/pic2.jpg","ano/pic3.jpg","ano/pic4.jpg");
+gamelist.push(ano);
+
+//add warcraft 3
+var wc3 = new Game("Warcraft III", "299000", "2GB", "RTS", "28/11/2004", "Blizzard", 
+        "CPU: Intel Core i3-550 | AMD Phenom II X4 955 or equivalent <br>\n RAM: 4 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 460 (1GB VRAM) | AMD Radeon HD 5770 (1GB VRAM) or equivalent <br>\n Hard Drive: 20 GB", 
+        "CPU: Intel Core i7-2600K | AMD FX-8350 or equivalent <br>\n RAM: 8 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 780 | AMD Radeon R9 280X or equivalent <br>\nHard Drive: 20 GB", 
+        "Warcraft III là trò chơi chiến lược thời gian thực do Blizzard phát triển xoay quanh những trận chiến giữa 4 chủng tộc chiến binh: chủng tộc con người (human); chủng tộc yêu tinh cây (night elf); chủng tộc chằn tinh (orc); chủng tộc bất tử (undead).",
+        "wc3/pic1.jpg","wc3/pic2.jpg","wc3/pic3.jpg","wc3/pic4.jpg");
+gamelist.push(wc3);
+
+//add lol
+var lol = new Game("LOL", "999000", "8GB", "RTS", "28/11/2010", "Riot", 
+        "CPU: Intel Core i3-550 | AMD Phenom II X4 955 or equivalent <br>\n RAM: 4 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 460 (1GB VRAM) | AMD Radeon HD 5770 (1GB VRAM) or equivalent <br>\n Hard Drive: 20 GB", 
+        "CPU: Intel Core i7-2600K | AMD FX-8350 or equivalent <br>\n RAM: 8 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 780 | AMD Radeon R9 280X or equivalent <br>\nHard Drive: 20 GB", 
+        "Liên Minh Huyền Thoại là trò chơi đấu trường trực tuyến. Mỗi trận đấu gồm 10 người chơi sẽ được chia làm hai đội gồm các vị tướng mạnh mẽ, mỗi tướng lại có lối chơi và thiết kế độc đáo, đối mặt nhau trên nhiều chiến trường và chế độ chơi.",
+        "lol/pic1.jpg","lol/pic2.jpg","lol/pic3.jpg","lol/pic4.jpg");
+gamelist.push(lol);
+
+//add assasin's creed
+var asc = new Game("Assassin's Creed", "799000", "14GB", "FPS", "28/11/2012", "Ubisoft", 
+        "CPU: Intel Core i3-550 | AMD Phenom II X4 955 or equivalent <br>\n RAM: 4 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 460 (1GB VRAM) | AMD Radeon HD 5770 (1GB VRAM) or equivalent <br>\n Hard Drive: 20 GB", 
+        "CPU: Intel Core i7-2600K | AMD FX-8350 or equivalent <br>\n RAM: 8 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 780 | AMD Radeon R9 280X or equivalent <br>\nHard Drive: 20 GB", 
+        "Assassin's Creed là một sê-ri trò chơi điện tử thể loại hành động phiêu lưu trong một thế giới mở có nội dung mang tính lịch sử-viễn tưởng. Với đồ họa đẹp tuyệt đỉnh, người chơi sẽ có cảm giác một sát thủ thực sự đang thực hiện những nhiệm vụ có 1 không 2",
+        "asc/pic1.jpg","asc/pic2.jpg","asc/pic3.jpg","asc/pic4.jpg");
+gamelist.push(asc);
+
+//add transformer
+var tfm = new Game("Transformer", "299000", "4GB", "FPS", "28/11/2007", "EA", 
+        "CPU: Intel Core i3-550 | AMD Phenom II X4 955 or equivalent <br>\n RAM: 4 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 460 (1GB VRAM) | AMD Radeon HD 5770 (1GB VRAM) or equivalent <br>\n Hard Drive: 20 GB", 
+        "CPU: Intel Core i7-2600K | AMD FX-8350 or equivalent <br>\n RAM: 8 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 780 | AMD Radeon R9 280X or equivalent <br>\nHard Drive: 20 GB", 
+        "Tranformer là bộ phim khoa học viễn tưởng nói về người máy ngoài hành tinh nổi tiếng trong giới điện ảnh. Không chỉ được xem những màn đánh nhau kịch tính, người chơi nay đã có thể trở thành 1 phần của bộ phim qua trò chơi Transformer đã chuyển thể.",
+        "tfm/pic1.jpg","tfm/pic2.jpg","tfm/pic3.jpg","tfm/pic4.jpg");
+gamelist.push(tfm);
+
+//add battlefield
+var btf = new Game("Battlefield 1", "899000", "23GB", "FPS", "28/11/2014", "EA", 
+        "CPU: Intel Core i3-550 | AMD Phenom II X4 955 or equivalent <br>\n RAM: 4 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 460 (1GB VRAM) | AMD Radeon HD 5770 (1GB VRAM) or equivalent <br>\n Hard Drive: 20 GB", 
+        "CPU: Intel Core i7-2600K | AMD FX-8350 or equivalent <br>\n RAM: 8 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 780 | AMD Radeon R9 280X or equivalent <br>\nHard Drive: 20 GB", 
+        "Battlefield 1 được xem là trò chơi thành công nhất trong việc mang đến cho game thủ cái nhìn về một trong những sự kiện lịch sử tàn khốc nhất thông qua cốt truyện chứa nhiều cảm xúc cũng như chế độ multiplayer hấp dẫn như thường lệ.",
+        "btf/pic1.jpg","btf/pic2.jpg","btf/pic3.jpg","btf/pic4.jpg");
+gamelist.push(btf);
+
+//add broforce
+var bro = new Game("Broforce", "199000", "1GB", "2DP", "28/11/2012", "EA", 
+        "CPU: Intel Core i3-550 | AMD Phenom II X4 955 or equivalent <br>\n RAM: 4 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 460 (1GB VRAM) | AMD Radeon HD 5770 (1GB VRAM) or equivalent <br>\n Hard Drive: 20 GB", 
+        "CPU: Intel Core i7-2600K | AMD FX-8350 or equivalent <br>\n RAM: 8 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 780 | AMD Radeon R9 280X or equivalent <br>\nHard Drive: 20 GB", 
+        "Thập niên những năm 80 khá nổi tiếng với các bộ phim hành động và táo bạo. Lấy cảm hứng từ thập niên này, Broforce mang đến cho người dùng các pha hành động ngộp thở nơi họ có thể hóa thân vào các nhân vật anh hùng của các thập niên 80 chống lại các kẻ khủng bố quá khích.",
+        "bro/pic1.jpg","bro/pic2.jpg","bro/pic3.jpg","bro/pic4.jpg");
+gamelist.push(bro);
+
+//add rayman
+var ray = new Game("Rayman Legends", "299000", "4GB", "2DP", "28/11/2015", "Ubisoft", 
+        "CPU: Intel Core i3-550 | AMD Phenom II X4 955 or equivalent <br>\n RAM: 4 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 460 (1GB VRAM) | AMD Radeon HD 5770 (1GB VRAM) or equivalent <br>\n Hard Drive: 20 GB", 
+        "CPU: Intel Core i7-2600K | AMD FX-8350 or equivalent <br>\n RAM: 8 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 780 | AMD Radeon R9 280X or equivalent <br>\nHard Drive: 20 GB", 
+        "Rayman là một trong những nhân vật kinh điển mà Ubisoft đã sáng tạo ra. Chàng siêu nhân với chân tay vô hình đã trải qua gần 20 năm với rất nhiều phiên bản và hệ máy. Duy chỉ có một điều không thay đổi, đó là sự hài hước, vui nhộn, thú vị và tràn ngập sắc màu tươi sáng trong mỗi tựa game",
+        "ray/pic1.jpg","ray/pic2.jpg","ray/pic3.jpg","ray/pic4.jpg");
+gamelist.push(ray);
+
+//add angrybird
+var agb = new Game("Angry Birds", "99000", "100MB", "2DP", "28/12/2009", "EA", 
+        "CPU: Intel Core i3-550 | AMD Phenom II X4 955 or equivalent <br>\n RAM: 4 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 460 (1GB VRAM) | AMD Radeon HD 5770 (1GB VRAM) or equivalent <br>\n Hard Drive: 20 GB", 
+        "CPU: Intel Core i7-2600K | AMD FX-8350 or equivalent <br>\n RAM: 8 GB <br>\n OS: Windows 7, Windows 8.1, Windows 10 (64-bit versions only) <br>\n Graphics: NVIDIA GeForce GTX 780 | AMD Radeon R9 280X or equivalent <br>\nHard Drive: 20 GB", 
+        "Angry Birds là một series trò chơi giải đố được phát hành lần đầu trên nền iOS của Apple và được tải về khoảng 12 triệu lần từ App Store. Angry Birds nhận được rất nhiều lời khen cho những sự kết hợp giữa cách chơi mới lạ, phong phú, phong cách truyện tranh và giá thành rẻ",
+        "agb/pic1.jpg","agb/pic2.jpg","agb/pic3.jpg","agb/pic4.jpg");
+gamelist.push(agb);
---
+++ function.js
@@ -0,0 +1,196 @@
+// data for my website
+var s;
+
+function Game(name, price, size, type, publishDate, maker, minimumRequirement, recommendRequirement, description, pic1, pic2, pic3, pic4) // constructor for creating a new game
+{
+    // data of class Game
+    this.name = name;
+    this.price = price;
+    this.size = size;
+    this.type = type;
+    this.publishDate = publishDate;
+    this.maker = maker;
+    this.minimumRequirement = minimumRequirement;
+    this.recommendRequirement = recommendRequirement;
+    this.description = description;
+    this.pic1 = pic1;
+    this.pic2 = pic2;
+    this.pic3 = pic3;
+    this.pic4 = pic4;
+    
+
+    // method of class Game
+    this.toString = function()
+    {
+        return  "\n<h1>" + this.name
+                + " <input type=\"button\" class=\"round\" value=\"" + this.type + "\">"
+                + "<br>\n" + (this.price*1).toLocaleString("en") + "đ</h1>\n"
+                // + "\n<h2>Publisher: " + this.maker + "<br>\nPublish date: " + this.publishDate + "<br>\nSize: " + this.size 
+                + "<input type=\"button\" class=\"square\" value=\"" + this.maker + "\">\n"
+                + "<input type=\"button\" class=\"square\" value=\"" + this.publishDate + "\">\n"
+                + "<input type=\"button\" class=\"square\" value=\"" + this.size + "\">"
+                + "<br><br><br>\n<h2>YÊU CẦU HỆ THỐNG:</h2>"
+                + "\n<p class=\"italic\"><u>Cấu hình tối thiểu</u><br>\n" + this.minimumRequirement 
+                + "<br>\n<u>Cấu hình đề nghị</u><br>\n" + this.recommendRequirement + "</p>"
+                + "\n<h2>MÔ TẢ:</h2>\n<p class=\"italic\">" + this.description + "</p>";
+
+    }    
+
+    this.toCard = function(desDiv, i)
+    {
+        var t = document.getElementById(desDiv);
+        t.innerHTML += 
+        "<div class=\"outer\"><div class=\"inner\" onclick=\"gamelist[" + i + "].toHTML('bot')\"><img src=\"" + this.pic1 + "\">"
+        + "<h2>" + this.name + "<br>" + (this.price*1).toLocaleString("en") + "đ</h2><p>Nhấp vào để xem chi tiết hoặc ấn vào nút bên dưới để đặt mua</p><input type=\"button\" class=\"button\" value=\"Đặt mua\" onclick=\"buythis("+ i +")\">"
+        + "</div></div>";   
+    }
+
+    this.toHTML = function(id)
+    {
+        document.body.style.height = "auto";
+        document.body.style.overflow = "visible";
+        var bot = document.getElementById("bot");
+        bot.style.display = "block";
+        bot.style.transform = "translateY(-100vh)";
+        var t = document.getElementById(id);
+        t.innerHTML = "<img src=\"" + this.pic2 + "\"><img src=\"" + this.pic3 + "\"><img src=\"" + this.pic4 + "\">";
+        var info = document.getElementById("info");
+        s = info.innerHTML;
+        info.innerHTML += this.toString();
+        info.style.opacity = 0.8;
+    }
+}
+
+function hideInfo()
+{
+    var info = document.getElementById("info");
+    info.style.opacity = 0;
+    info.innerHTML = s;
+    var bot = document.getElementById("bot");
+    bot.style.transform = "translateY(0px)";
+    document.body.style.height = "100vh";
+    document.body.style.overflow = "hidden";    
+}
+
+function showGameList()
+{
+    var t = document.getElementById("optionlist");
+    for (var i=0; i < gamelist.length; i++)
+    {
+        t.innerHTML += "\n<option value=\"" + i + "\">" + gamelist[i].name + "</option>";    
+    }
+    var u = window.location.href;
+    var n = "";
+    var i = u.length - 1;
+    if (u[i] == "l")
+    {
+        t.selectedIndex = -1;
+    }
+    else
+    {
+        while (u[i] != "?" && u[i]!="l") 
+        {
+            n = u[i] + n;
+            i--;
+        }
+        t.selectedIndex = new Number(n);
+    }
+}
+
+function tinhTien()
+{
+    var t = document.getElementById("tongtien");
+    var g = document.getElementById("optionlist");
+    var sl = document.getElementById("sl");
+    t.innerHTML = (gamelist[g.selectedIndex].price * sl.value).toLocaleString("vi") + " VND";
+}
+
+function buythis(i)
+{
+    window.open("order.html?" + i, "_self");
+}
+
+function loadCard()
+{
+    for (var i=0; i < gamelist.length; i++)
+    {
+        gamelist[i].toCard("top", i);
+    }
+}
+
+function loadFPSCard()
+{
+    for (var i=0; i < gamelist.length; i++)
+    {
+        if (gamelist[i].type == "FPS")
+        {
+            gamelist[i].toCard("top", i);   
+        }
+    }
+}
+
+function loadRTSCard()
+{
+    for (var i=0; i < gamelist.length; i++)
+    {
+        if (gamelist[i].type == "RTS")
+        {
+            gamelist[i].toCard("top", i);   
+        }
+    }
+}
+
+function loadPLTCard()
+{
+    for (var i=0; i < gamelist.length; i++)
+    {
+        if (gamelist[i].type == "2DP")
+        {
+            gamelist[i].toCard("top", i);   
+        }
+    }
+}
+
+function blurBG()
+{
+    var t = document.getElementById("bgvid");
+    window.setTimeout(function()
+    {
+        t.style.opacity = "0.3";
+    }, 8000);
+}
+
+function show345()
+{
+    if (window.innerWidth > 800)
+    {
+        tab3.style.transform = "translateX(0px)";
+        tab4.style.transform = "translateX(0px)";
+        tab5.style.transform = "translateX(0px)";
+        tab6.style.top = "290px";
+        
+    }
+}
+
+function hide345()
+{
+    if (window.innerWidth > 800)
+    {
+        tab3.style.transform = "translateX(-100px)";
+        tab4.style.transform = "translateX(-100px)";
+        tab5.style.transform = "translateX(-100px)";
+        tab6.style.top = "130px";   
+    }
+}
+
+function focuson(which)
+{
+    var tab = document.getElementById("tab" + which);
+    tab1.style.color = "white";
+    tab2.style.color = "white";
+    tab3.style.color = "white";
+    tab4.style.color = "white";
+    tab5.style.color = "white";
+    tab6.style.color = "white";
+    tab.style.color = "#8EC127";
+}
